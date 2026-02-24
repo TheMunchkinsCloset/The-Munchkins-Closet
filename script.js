cdb26ba6e25f24267d44694e1461fd3cc2dcc8a9
brandinpotter <Brandinpotter@gmail.com>
2026-02-23 21:36:02 -0500

    c

---
+++ script.js
@@ -0,0 +1,11 @@
+const DonationsBtn = document.getElementById("Donations");
+
+btn.addEventListener("click", () => {
+  window.location.href = "Donations/";
+});
+
+const ContactsbBtn = document.getElementById("Contacts");
+
+btn.addEventListener("click", () => {
+  window.location.href = "Contact Info/";
+});
